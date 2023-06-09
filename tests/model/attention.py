a514e54c6e022bbd0a732b63b961defbf83c8804
Ayaka Mikazuki <ayaka@mail.shn.hk>
2023-06-09 14:52:40 +0800

    Update

--- tests/model/attention.py
+++ tests/model/attention.py
@@ -2,22 +2,28 @@ from pathlib import Path; import sys; sys.path.append(str(Path(__file__).resolve
 from lib.proc_init_utils import initialise_cpu; initialise_cpu()
 
 import jax.numpy as jnp
-import numpy as np
 import torch
-import torch.nn as tnn
 from transformers import LlamaConfig
 from transformers.models.llama.modeling_llama import LlamaAttention
 
 from lib.array_utils import pt2jax
-from lib.model import config_7B, Attention, attention
+from lib.model import config_7B, attention
+from lib.param_utils import convert_attention
 
-batch_size = 1  # TODO: change to 2 to test masking
-seq_len = 9
+batch_size = 2  # TODO: change to 2 to test masking
+seq_len = 7
 
 config_pt = LlamaConfig()  # default to LLaMA 7B
 
 attention_pt = LlamaAttention(config=config_pt)
 
-params_jax = convert_attention()
+params_jax = convert_attention(attention_pt, config=config_7B)
 
-config_7B
+# initialise input sequence
+seq_pt = torch.rand(batch_size, seq_len, config_7B.d_model)
+seq_jax = pt2jax(seq_pt)
+
+y_pt = attention_pt(hidden_states=seq_pt, attention_mask=None)[0]
+y_jax = pt2jax(y_pt)
+y_head_jax = attention(params_jax, seq_jax, seq_jax, None, config=config_7B)
+assert jnp.allclose(y_jax, y_head_jax)
--- tests/model/rms_norm.py
+++ tests/model/rms_norm.py
@@ -11,16 +11,14 @@ from lib.model import config_7B, rms_norm
 
 batch_size = 2
 seq_len = 9
-d_model = config_7B.d_model
-rms_norm_eps = config_7B.rms_norm_eps
 
-rms_norm_pt = LlamaRMSNorm(hidden_size=d_model, eps=rms_norm_eps)
+rms_norm_pt = LlamaRMSNorm(hidden_size=config_7B.d_model, eps=config_7B.rms_norm_eps)
 rms_norm_pt.weight = tnn.Parameter(torch.randn_like(rms_norm_pt.weight))
 
 params_pt = rms_norm_pt.weight
 params_jax = pt2jax(params_pt)
 
-x_pt = torch.rand(batch_size, seq_len, d_model)
+x_pt = torch.rand(batch_size, seq_len, config_7B.d_model)
 x_jax = pt2jax(x_pt)
 
 y_pt = rms_norm_pt(x_pt)

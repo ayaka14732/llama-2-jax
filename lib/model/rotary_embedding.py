fa7f30a97a70ffd26fbda46414a374fa1bad8393
Ayaka Mikazuki <ayaka@mail.shn.hk>
2023-06-22 20:57:42 +0800

    Update

--- lib/model/rotary_embedding.py
+++ lib/model/rotary_embedding.py
@@ -4,13 +4,13 @@ import jax.numpy as jnp
 
 # TODO: Mostly taken from https://github.com/kingoflolz/mesh-transformer-jax/blob/master/mesh_transformer/layers.py
 # and https://github.com/huggingface/transformers/blob/main/src/transformers/models/llama/modeling_llama.py#L92
-def make_weights(seq_len, d_kv):
-    inv_freq = 1. / (10000 ** (jnp.arange(0, d_kv, 2) / d_kv))
+def make_weights(seq_len, d_k):
+    inv_freq = 1. / (10000 ** (jnp.arange(0, d_k, 2) / d_k))
     sinusoid_inp = op.einsum(jnp.arange(seq_len), inv_freq, 'seq_len, j -> seq_len j')
     sin_val = jnp.sin(sinusoid_inp)
     cos_val = jnp.cos(sinusoid_inp)
-    sin_val = op.repeat(sin_val, 'seq_len d_kv -> seq_len (i d_kv)', i=2)
-    cos_val = op.repeat(cos_val, 'seq_len d_kv -> seq_len (i d_kv)', i=2)
+    sin_val = op.repeat(sin_val, 'seq_len d_k -> seq_len (i d_k)', i=2)
+    cos_val = op.repeat(cos_val, 'seq_len d_k -> seq_len (i d_k)', i=2)
     return sin_val, cos_val
 
 def rotate_every_two(x):
@@ -19,16 +19,15 @@ def rotate_every_two(x):
     x = op.rearrange(x, '... x i -> ... (x i)')
     return x
 
-def rotary_embedding(x):
+def rotary_embedding(m):
     # assert config.d_k % 2 == 0
-    # assert config.d_v % 2 == 0
 
-    *_, seq_len, d_kv = x.shape  # `d_kv`: either `d_k` or `d_v` depending on the context
+    *_, seq_len, d_k = m.shape
 
     with jax.ensure_compile_time_eval():
-        sin_val, cos_val = make_weights(seq_len, d_kv)
+        sin_val, cos_val = make_weights(seq_len, d_k)
 
-    pattern = 'batch_size n_heads seq_len d_kv, seq_len d_kv -> batch_size n_heads seq_len d_kv'
-    a = op.einsum(x, cos_val, pattern)
-    b = op.einsum(rotate_every_two(x), sin_val, pattern)
+    n = rotate_every_two(m)
+    a = op.einsum(m, cos_val, 'batch_size n_heads seq_len d_k, seq_len d_k -> batch_size n_heads seq_len d_k')
+    b = op.einsum(n, sin_val, 'batch_size n_heads seq_len d_k, seq_len d_k -> batch_size n_heads seq_len d_k')
     return a + b
--- tests/model/rotary_embedding.py
+++ tests/model/rotary_embedding.py
@@ -9,15 +9,15 @@ from lib.model.rotary_embedding import make_weights
 
 batch_size = 2
 seq_len = 2048
-d_kv = 512
+d_k = 512
 
-rotary_embedding_pt = LlamaRotaryEmbedding(dim=d_kv, max_position_embeddings=seq_len)
+rotary_embedding_pt = LlamaRotaryEmbedding(dim=d_k, max_position_embeddings=seq_len)
 sin_val_pt = rotary_embedding_pt.sin_cached
 cos_val_pt = rotary_embedding_pt.cos_cached
 sin_val_jax = pt2jax(sin_val_pt)
 cos_val_jax = pt2jax(cos_val_pt)
 
-sin_val_hat_jax, cos_val_hat_jax = make_weights(seq_len, d_kv)
+sin_val_hat_jax, cos_val_hat_jax = make_weights(seq_len, d_k)
 
 assert jnp.allclose(sin_val_jax.squeeze(), sin_val_hat_jax)
 assert jnp.allclose(cos_val_jax.squeeze(), cos_val_hat_jax)

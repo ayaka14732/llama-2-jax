fdbd921d9fa6ae1a3cc34369644a570c6969ff3a
Ayaka <ayaka@mail.shn.hk>
2023-07-20 20:02:37 +0800

    1

--- train.py
+++ train.py
@@ -57,7 +57,7 @@ def main() -> None:
     collate_fn = partial(gsm_collate_fn_train, tokenizer, max_len)
     dataloader = LlamaDataLoader(dataset, collate_fn, batch_size, seed)
 
-    params = load_params('/dev/shm/llama2-7B-float16.pickle')
+    params = load_params('llama2-7B-float16.pickle')
 
     optimizer = optax.adafactor(learning_rate=lr)
     optimize = optimizer.update

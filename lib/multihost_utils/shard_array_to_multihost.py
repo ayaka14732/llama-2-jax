d10fba3562a326ca25e67b75c4bc35e9826d70c3
Ayaka Mikazuki <ayaka@mail.shn.hk>
2023-07-24 00:54:12 +0800

    update

---
+++ lib/multihost_utils/shard_array_to_multihost.py
@@ -0,0 +1,24 @@
+import jax
+from jax import Array
+from jax.sharding import Mesh, NamedSharding, PartitionSpec as P
+import numpy as np
+from types import EllipsisType
+
+def shard_array_to_multihost(arr: Array, axis: int | EllipsisType) -> Array:
+    shape = arr.shape
+    devices: np.ndarray = np.array(jax.devices())
+
+    if axis is ...:
+        mesh = Mesh(devices, ('a',))
+        sharding = NamedSharding(mesh, P(None))
+    else:
+        sharding_tuple_ = [1] * len(shape)
+        sharding_tuple_[axis] = -1
+        sharding_tuple = tuple(sharding_tuple_)
+        name_tuple = tuple('abcdefghijklmnopqrstuvwxyz'[:len(shape)])
+
+        mesh = Mesh(devices.reshape(sharding_tuple), name_tuple)
+        sharding = NamedSharding(mesh, P(*name_tuple))
+
+    arrays = [jax.device_put(arr[i], device) for device, i in sharding.addressable_devices_indices_map(shape).items()]
+    return jax.make_array_from_single_device_arrays(shape, sharding, arrays)
--- lib/tree_utils/__init__.py
+++ lib/tree_utils/__init__.py
@@ -1,13 +1,3 @@
-import jax
-import jax.numpy as jnp
-
-# https://docs.liesel-project.org/en/v0.1.4/_modules/liesel/goose/pytree.html#stack_leaves
-def stack_leaves(pytrees, axis: int=0):
-    return jax.tree_util.tree_map(lambda *xs: jnp.stack(xs, axis=axis), *pytrees)
-
-# https://gist.github.com/willwhitney/dd89cac6a5b771ccff18b06b33372c75?permalink_comment_id=4634557#gistcomment-4634557
-def unstack_leaves(pytrees):
-    leaves, treedef = jax.tree_util.tree_flatten(pytrees)
-    return [treedef.unflatten(leaf) for leaf in zip(*leaves, strict=True)]
-
+from .stack_leaves import stack_leaves
 from .tree_apply import tree_apply
+from .unstack_leaves import unstack_leaves

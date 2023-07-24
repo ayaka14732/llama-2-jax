66d984f5b2083873e50d8685897785bba7d8e07d
Ayaka <ayaka@mail.shn.hk>
2023-07-24 19:15:33 +0800

    1

--- determine_max_length.py
+++ determine_max_length.py
@@ -1,6 +1,7 @@
 from pathlib import Path; import sys; sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
 from lib.proc_init_utils import initialise_cpu; initialise_cpu()
 
+import matplotlib.pyplot as plt
 from transformers import LlamaTokenizer
 
 from lib.gsm_data.GSMDataset import load_data
@@ -14,9 +15,17 @@ for question, answer in data:
     tokenized_ids = tokenizer(question + answer).input_ids
     len_list.append(len(tokenized_ids))
 
-print(max(len_list))
+print('Max length:', max(len_list))
 
-cutoff = 512
-n_total = len(data)
-n_cut = sum(length > cutoff for length in len_list)
-print(f'n_cut: {n_cut}, n_total {n_total}')
+max_val = max(len_list) + 25
+
+plt.figure(figsize=(10, 6))
+plt.hist(len_list, bins=range(0, max_val, 25), edgecolor='black')
+plt.title('GSM Data Length Distribution')
+plt.xlabel('Ranges (Every 25)')
+plt.ylabel('Count')
+plt.xticks(range(0, max_val, 25))
+plt.grid(True)
+
+plt.tight_layout()
+plt.savefig('len_dist.png')

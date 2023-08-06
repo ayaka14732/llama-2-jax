b8aa5d33848ba8156b6050efccf88c838458bb91
Ayaka <ayaka@mail.shn.hk>
2023-08-06 22:51:19 +0800

    Update

--- lib/llama/__init__.py
+++ lib/llama/__init__.py
@@ -1,3 +1,3 @@
 from .ModelConfig import ModelConfig, model_config_llama1_7B, model_config_llama2_70B, model_config_llama2_7B
-from .llama import Llama, check_llama, forward_llama
-from .llama_model import LlamaModel, check_llama_model, forward_llama_model
+from .llama import Llama, check_llama, forward_llama, init_llama
+from .llama_model import LlamaModel, check_llama_model, forward_llama_model, init_llama_model

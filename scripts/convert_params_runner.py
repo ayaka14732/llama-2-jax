334e08b1ee040e980e9c2c9a814bf5d25cc23bf8
Ayaka <ayaka@mail.shn.hk>
2023-10-05 05:31:41 +0000

    Support Llama 2 13B

--- scripts/convert_params_runner.py
+++ scripts/convert_params_runner.py
@@ -6,13 +6,14 @@ import jax
 import jax.numpy as jnp
 from transformers import LlamaForCausalLM
 
-from lib.llama import check_llama, model_config_llama1_7B, model_config_llama2_70B, model_config_llama2_7B
+from lib.llama import check_llama, model_config_llama1_7B, model_config_llama2_13B, model_config_llama2_70B, model_config_llama2_7B
 from lib.llama_params import convert_llama
 from lib.param_utils import save_params
 
 pairs = {
     'llama1-7B': ('../llama-weights/llama1-7B', model_config_llama1_7B),
     'llama2-7B': ('meta-llama/Llama-2-7b-hf', model_config_llama2_7B),
+    'llama2-13B': ('meta-llama/Llama-2-13b-hf', model_config_llama2_13B),
     'llama2-70B': ('meta-llama/Llama-2-70b-hf', model_config_llama2_70B),
 }
 

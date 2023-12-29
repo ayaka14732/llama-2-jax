c7bace9bb2d7007fc60be56b0c9fc248fed6729e
Divya Patel <divyapatel0273@gmail.com>
2023-12-29 16:13:00 +0530

    update

--- scripts/convert_params_runner.py
+++ scripts/convert_params_runner.py
@@ -23,7 +23,7 @@ pairs = {
 
 def convert(target: str, save_path: str = '') -> None:
     path, model_config = pairs[target]
-    model_pt = LlamaForCausalLM.from_pretrained(path, torch_dtype=torch.bfloat16) 
+    model_pt = LlamaForCausalLM.from_pretrained(path, torch_dtype= torch.float16)
     params = convert_llama(model_pt, model_config=model_config)
     params = jax.tree_map(lambda x: x.astype(jnp.bfloat16), params)
     check_llama(params, model_config=model_config)

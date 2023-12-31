0f52cbdeb4e61265e371dc01f6ed4cc731955802
Divya Patel <divyapatel0273@gmail.com>
2024-01-01 01:12:19 +0530

    updates

--- scripts/convert_params_runner.py
+++ scripts/convert_params_runner.py
@@ -1,15 +1,19 @@
-from pathlib import Path; import sys; sys.path.append(str(Path(__file__).resolve().parent.parent))
-from lib.proc_init_utils import initialise_cpu; initialise_cpu()
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+from lib.proc_init_utils import initialise_cpu
+initialise_cpu()
 from huggingface_hub import HfApi
 import torch
 import fire
 import jax
-import jax.numpy as jnp
 from transformers import LlamaForCausalLM
 import gc
 from lib.llama import check_llama, model_config_llama1_7B, model_config_llama2_13B, model_config_llama2_70B, model_config_llama2_7B
 from lib.llama_params import convert_llama
 from lib.param_utils import save_params
+import aqt.jax.v2.flax.aqt_flax as aqt
+import aqt.jax.v2.config as aqt_config
 
 pairs = {
     'llama1-7B': ('../llama-weights/llama1-7B', model_config_llama1_7B),
@@ -23,11 +27,13 @@ pairs = {
 
 def convert(target: str, save_path: str = '') -> None:
     path, model_config = pairs[target]
-    model_pt = LlamaForCausalLM.from_pretrained(path, torch_dtype= torch.uint8)
+    model_pt = LlamaForCausalLM.from_pretrained(path, torch_dtype= torch.float16)
     params = convert_llama(model_pt, model_config=model_config)
     del model_pt
+    
     gc.collect()
-    params = jax.tree_map(lambda x: x.astype(jnp.uint8), params)
+    # Quantize the parameters to 8-bit using AQT
+    params = aqt.quantize(params, num_bits=8)
     check_llama(params, model_config=model_config)
 
     print(f'Converted parameters for {target}')
@@ -52,7 +58,5 @@ def convert(target: str, save_path: str = '') -> None:
         repo_type="model",
     )
 
-
 if __name__ == '__main__':
   fire.Fire(convert)
-

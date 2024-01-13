7f2e998ce0967c6bb5f357781a7a7845229f44fd
Divya Patel <divyapatel0273@gmail.com>
2024-01-13 12:24:49 +0530

    update

--- lib/llama/ModelConfig.py
+++ lib/llama/ModelConfig.py
@@ -52,6 +52,40 @@ model_config_llama1_7B = ModelConfig(
     return_kv_cache=False,
 )
 
+model_config_llama1_13B = ModelConfig(
+    d_ff=13824,
+    d_k=128,
+    d_model=5120,
+    d_v=128,
+    n_heads_kv=40,
+    n_layers=40,
+    n_rep_kv=1,
+    rms_norm_eps=1e-6,
+    token_id_bos=1,
+    token_id_eos=2,
+    token_id_pad=0,
+    vocab_size=32000,
+    dropout_rate=0.1,
+    return_kv_cache=False,
+)
+
+model_config_llama1_30B = ModelConfig(
+    d_ff=28672,
+    d_k=128,
+    d_model=8192,
+    d_v=128,
+    n_heads_kv=8,
+    n_layers=80,
+    n_rep_kv=8,
+    rms_norm_eps=1e-6,
+    token_id_bos=1,
+    token_id_eos=2,
+    token_id_pad=0,
+    vocab_size=32000,
+    dropout_rate=0.1,
+    return_kv_cache=False,
+)
+
 model_config_llama2_7B = model_config_llama1_7B
 
 model_config_llama2_13B = ModelConfig(
